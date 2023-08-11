fa9302a17debeb02516792e931aca9b2aa6ca44c
johirjoy <joy.sd69@gmail.com>
2023-08-11 16:26:54 +0600

    components, navbar and footer added

--- src/layouts/Main.jsx
+++ src/layouts/Main.jsx
@@ -1,9 +1,13 @@
 import { Outlet } from "react-router-dom";
+import Navbar from "../components/Shared/Navbar";
+import Footer from "../components/Shared/Footer";
 
 const Main = () => {
     return (
         <div>
+            <Navbar />
             <Outlet />
+            <Footer />
         </div>
     );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import { RouterProvider } from 'react-router-dom'
-import {router} from './routes/Route'
+import { router } from './routes/Route'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <div className='container mx-auto'>
+      <RouterProvider router={router} />
+    </div>
   </React.StrictMode>,
 )
